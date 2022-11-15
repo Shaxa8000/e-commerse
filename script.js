d1f4159696c0c01aadeebd4f8ad9dc925f59ed9d
Shaxboz <shaxboz.yaxyoyev@bk.ru>
2022-11-15 18:15:40 +0500

    cart open close done

--- script.js
+++ script.js
@@ -85,6 +85,7 @@ class UI {
                 // display cart item
                 this.addCartItem(cartItem)
                 // show the cart
+                this.showCart()
             })
           }
         )
@@ -103,18 +104,39 @@ class UI {
         const div = document.createElement('div');
         div.classList.add('cart-item');
         div.innerHTML = `
-            <img src="" alt="product">
+            <img src=${item.image} alt="product">
             <div>
-                <h4>queen bed</h4>
-                <h5>$9.00</h5>
-                <span class="remove-item">remove</span>
+                <h4>${item.title}</h4>
+                <h5>$${item.price}</h5>
+                <span class="remove-item" data-id=${item.id} >remove</span>
             </div>
             <div>
-                <i class="fas fa-chevron-up"></i>
-                <p class="item-amount">1</p>
-                <i class="fas fa-chevron-down"></i>
+                <i class="fas fa-chevron-up" data-id=${item.id}></i>
+                <p class="item-amount">${item.amount}</p>
+                <i class="fas fa-chevron-down" data-id=${item.id}></i>
             </div>
         `
+        cartContent.appendChild(div);
+    };
+    showCart() {
+        cartOverlay.classList.add('transparentBcg');
+        cartDOM.classList.add('showCart');
+    };
+    setupAPP() {
+        cart = Storage.getCart();
+        this.setCartValues(cart);
+        this.populateCart(cart);
+        cartBtn.addEventListener('click', this.showCart);
+        closeCartBtn.addEventListener('click', this.hideCart)
+
+    };
+    populateCart(cart) {
+        cart.forEach(item => this.addCartItem(item));
+    };
+
+    hideCart() {
+         cartOverlay.classList.remove('transparentBcg');
+         cartDOM.classList.remove('showCart');
     }
 }
 
@@ -127,14 +149,20 @@ class Storage {
      let products = JSON.parse(localStorage.getItem('products'));
      return products.find(product => product.id === id);
     }
-static saveCart(cart) {
+ static saveCart(cart) {
     localStorage.setItem('cart', JSON.stringify(cart))
+    }
+ static getCart() {
+     return localStorage.getItem('cart') ? JSON.parse(localStorage.getItem('cart')) : [];
 }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     const ui = new UI();
     const products = new Products();
+
+    // setup App
+    ui.setupAPP();
 
     // get all products
     products.getProducts().then(products => {
